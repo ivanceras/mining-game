42b87dc1bea7e25a387f84e0327423867bbc4481
Jovansonlee Cesar <ivanceras@gmail.com>
2022-02-13 15:03:02 +0800

    fix: no need to multiply the direction to camera rotation it is already transformed to the camera orientation when creating a ray from screenspace

--- src/projectile.rs
+++ src/projectile.rs
@@ -42,14 +42,13 @@ pub(crate) fn spawn_projectile(
     if keys.pressed(KeyCode::LShift) && mouse_buttons.pressed(MouseButton::Left) {
         let mouse_ray = selector::from_screenspace(windows, camera, camera_rig).unwrap();
         for camera_transform in query.iter() {
-            let direction = if PROJECTILE_FROM_CENTER {
+            let direction: Vec3 = if PROJECTILE_FROM_CENTER {
                 camera_transform.forward()
             } else {
-                let direction: Vec3 = mouse_ray.dir.into();
-                camera_transform.rotation * direction
+                mouse_ray.dir.into()
             };
 
-            let offset = 10.0; //offset meters away in front of the camera
+            let offset = 1.0; //offset meters away in front of the camera
             commands
                 .spawn_bundle(PbrBundle {
                     mesh: meshes.add(Mesh::from(shape::Icosphere {
